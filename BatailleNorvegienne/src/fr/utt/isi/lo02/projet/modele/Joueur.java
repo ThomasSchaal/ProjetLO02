f4ae3be535591a731ec60081a902f4d8909165d3
ThomasSchaal <thomas.schaal068@gmail.com>
2014-11-29 17:27:46 +0100

    Création constructeur Joueur avec main/CarteCachee/CarteVisible

--- BatailleNorvegienne/src/fr/utt/isi/lo02/projet/modele/Joueur.java
+++ BatailleNorvegienne/src/fr/utt/isi/lo02/projet/modele/Joueur.java
@@ -5,38 +5,51 @@ import java.util.Scanner;
 public class Joueur {
 	protected int idJoueur;
 	protected String nomJoueur;
-	
-	// TODO rajouter le nom au joueur 
-	public Joueur(int idJoueur){
-		this.idJoueur=idJoueur;
-		//this.nomJoueur=nomJoueur;
+	private Main main;
+	private CarteCachee carteCachee;
+	private CarteVisible carteVisible;
+
+	// TODO rajouter le nom au joueur
+	public Joueur(int idJoueur) {
+		this.idJoueur = idJoueur;
+		// this.nomJoueur=nomJoueur;
 	}
-	
-	public static int choisirNbJoueur(){
+
+	public Joueur(Main m, CarteCachee cc, CarteVisible cv) {
+		this.main = m;
+		this.carteCachee = cc;
+		this.carteVisible = cv;
+	}
+
+	public static int choisirNbJoueur() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez choisir un nombre de joueurs entre 1 et 10 :");
+		System.out
+				.println("Veuillez choisir un nombre de joueurs entre 1 et 10 :");
 		int nbJoueur = sc.nextInt();
 		System.out.println("Vous avez saisi : " + nbJoueur);
-		
-		if(nbJoueur > 10){
+
+		if (nbJoueur > 10) {
 			System.out.println("Entre 1 et 10, c'est pas compliqué non ? ");
-			choisirNbJoueur(); 
+			choisirNbJoueur();
 		}
 		return nbJoueur;
-	} 
-	
-	public static Joueur[] listJoueur(){
+	}
+
+	public static Joueur[] listJoueur() {
 		int nbJoueur = choisirNbJoueur();
 		Joueur[] tabJoueur = new Joueur[nbJoueur];
-		for(int i = 0 ; i<nbJoueur ; i++){
+		for (int i = 0; i < nbJoueur; i++) {
 			Joueur j = new Joueur(i);
-			tabJoueur[i]= j;
+			tabJoueur[i] = j;
 			System.out.println(tabJoueur[i]);
 		}
-		return tabJoueur; 	
+		return tabJoueur;
 	}
-	public void echangerLesCartes(Carte carte1, Carte carte2){
-		
-	}
+
+	// TODO créer méthode pour mettre les cartes dans la main/carteCachee/carteVisible en reception de la distribution
 	
+	public void echangerLesCartes(Carte carte1, Carte carte2) {
+
+	}
+
 }
